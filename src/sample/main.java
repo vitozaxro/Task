97b2c0f056d9aa6e8c5efd2bd675715790b93dff
vito.zaqaraia <vito.zaqaraia@gmail.com>
2020-10-15 17:20:50 +0400

    da

--- src/sample/main.java
+++ src/sample/main.java
@@ -10,7 +10,7 @@ public class main extends Application {
     @Override
     public void start(Stage primaryStage) throws Exception{
         Parent root = FXMLLoader.load(getClass().getResource("sample.fxml"));
-        primaryStage.setTitle("Hello World");
+        primaryStage.setTitle("Hello me");
         primaryStage.setScene(new Scene(root, 300, 275));
         primaryStage.show();
     }
